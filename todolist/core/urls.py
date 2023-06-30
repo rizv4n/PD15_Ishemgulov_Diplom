5900bfac5ec57b6447a7326446b3a166537c7a9b
rizv4n <rizvanishemgulov@gmail.com>
2023-06-30 15:14:07 +0500

    third_commit_fixed_2.0

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from core.views import UserCreateView, UserLoginView, UserRetrieveUpdateView, UpdatePasswordView
 
 urlpatterns = [
-    path('signup/', UserCreateView.as_view(), name='signup'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserRetrieveUpdateView.as_view(), name='retrieve'),
-    path('update_password/', UpdatePasswordView.as_view(), name='update_password')
+    path('signup', UserCreateView.as_view(), name='signup'),
+    path('login', UserLoginView.as_view(), name='login'),
+    path('profile', UserRetrieveUpdateView.as_view(), name='retrieve'),
+    path('update_password', UpdatePasswordView.as_view(), name='update_password')
 ]
