c67477769601735cb82c5f86758661174ed93391
rizv4n <rizvanishemgulov@gmail.com>
2023-07-03 14:10:35 +0500

    commit_4.0

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from core import urls as core_urls
+from goals import urls as goal_urls
 from social_django import urls as social_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include(core_urls)),
+    path('goals/', include(goal_urls)),
     path('oauth/', include(social_urls, namespace='social')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
